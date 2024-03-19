5786597f4caa4b27fe036de70eee0ac9817a14d9
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2024-03-19 12:42:42 -0700

    adding fetch

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
-alert("I am linked")
+fetch("/api/pets").then(res=>res.json()).then(petData=>{
+    console.log(petData)
+    const petsUl = document.querySelector("#pets");
+    petData.forEach(pet=>{
+        const petLi = document.createElement("li");
+        petLi.textContent = `id: ${pet.id}.  Hi I am a ${pet.species} named ${pet.name}`;
+        petsUl.append(petLi)
+    })
+}).catch(err=>{
+    console.log("womp womp")
+    console.log(err);
+})
