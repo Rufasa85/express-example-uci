ea520ff212561edf7ba33bd3f36774d79f5527a0
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2024-03-20 10:18:24 -0700

    adding front end fetch post

--- public/js/script.js
+++ public/js/script.js
@@ -10,3 +10,25 @@ fetch("/api/pets").then(res=>res.json()).then(petData=>{
     console.log("womp womp")
     console.log(err);
 })
+
+document.querySelector("#new-pet-form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const petObj = {
+        name: document.querySelector("#new-pet-name").value,
+        species: document.querySelector("#new-pet-species").value,
+    }
+    console.log(petObj)
+    fetch("/api/pes",{
+        method:"POST",
+        body:JSON.stringify(petObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+           location.reload()
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
